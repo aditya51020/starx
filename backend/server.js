85bd1761d10dc434f56bc20fccffb409d0af6969
Aditya Rai <adityarai51020@gmail.com>
2026-01-31 18:12:16 +0530

    Increase request body limit to 50MB to fix upload 413 error

--- backend/server.js
+++ backend/server.js
@@ -62,7 +62,8 @@ app.use('/api', limiter);
 app.use(morgan('dev'));
 
 // Body & Cookie parsers
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser());
 
 // Routes
