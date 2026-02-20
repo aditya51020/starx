3edf22bf0b9510df73fa7a24ec5ff1133eb79f3a
Aditya Rai <adityarai51020@gmail.com>
2026-02-20 19:52:32 +0530

    fix: remove region isIn validation to allow any location

--- backend/models/Property.js
+++ backend/models/Property.js
@@ -28,10 +28,7 @@ const Property = sequelize.define('Property', {
   },
   region: {
     type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-      isIn: [['Vasundhara', 'Indirapuram', 'Sector 63', 'Vaishali', 'Noida Extension', 'Sahibabad', 'Siddharth Vihar', 'Crossings Republik', 'Raj Nagar Extension', 'Govindpuram', 'Other']]
-    }
+    allowNull: false
   },
   propertyType: {
     type: DataTypes.STRING,
